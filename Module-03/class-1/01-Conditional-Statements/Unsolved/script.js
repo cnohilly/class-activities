d1b4a61899d190ad38518d8574ef4e4284c78e7a
ctnohilly <ctnohilly@gmail.com>
2022-05-17 21:18:38 -0400

    Tuesday 5-17-22 Class

--- Module-03/class-1/01-Conditional-Statements/Unsolved/script.js
+++ Module-03/class-1/01-Conditional-Statements/Unsolved/script.js
@@ -1,6 +1,35 @@
 // TODO: Change the values and operators below to test your algorithm meets all conditions
-var x = 50;
+var x = window.prompt("What is your number?");;
 var expression1 = x < 25;
 var expression2 = x > 50;
 
 // TODO: Write Your JavaScript Code Here
+// It is done when the message "True ✅ True ✅" is logged when both `expression1` and `expression2` are true.
+if (expression1 && expression2){
+    console.log("True ✅ True ✅");
+}
+// It is done when the message "True ✅ False ❌" is logged when `expression1` is true.
+ else if (expression1){
+    console.log("True ✅ False ❌");
+}
+// It is done when the message "False ❌ True ✅" is logged when `expression2` is true.
+ else if (expression2){
+    console.log("False ❌ True ✅");
+}
+// It is done when the message "False ❌ False ❌" is logged when both `expression1` and `expression2` are false.
+ else {
+    console.log("False ❌ False ❌");
+}
+
+
+
+switch (true) {
+    case expression1 && expression2: console.log("True True");
+        break;
+    case expression1: console.log("True False");
+        break;
+    case expression2: console.log("False True");
+        break;
+    default:
+        console.log("False False");
+}
--- Module-03/class-1/02-Iteration/Unsolved/script.js
+++ Module-03/class-1/02-Iteration/Unsolved/script.js
@@ -1,1 +1,10 @@
 // TODO: Write Your JavaScript Code Here
+var students = ["Chris","Ati","Braulio","Liv","Lakshmi","Marvan","Caid","Daniel","Valerie"];
+
+for (var i = 0; i < students.length; ++i){
+    console.log("Great to see you, " + students[i] + "!");
+}
+
+while(students.length > 0){
+    console.log("Great to see you, " + students.shift());
+}
