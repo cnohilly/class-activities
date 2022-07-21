70dced8eb8e9656e0cbd2107abee0fd2e5eca7d9
ctnohilly <ctnohilly@gmail.com>
2022-07-21 19:11:09 -0400

    tuesday 7/19

--- Module-12/class-1/01-SQL-Schema/Unsolved/db/schema.sql
+++ Module-12/class-1/01-SQL-Schema/Unsolved/db/schema.sql
@@ -1,2 +1,13 @@
 DROP DATABASE IF EXISTS inventory_db;
 CREATE DATABASE inventory_db;
+USE inventory_db;
+CREATE TABLE inventory (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    item_name VARCHAR(30) NOT NULL,
+    stock INTEGER NOT NULL
+);
+CREATE TABLE special_inventory (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    item_name VARCHAR(30) NOT NULL,
+    stock INTEGER NOT NULL
+);
---
+++ Module-12/class-1/02-SQL-Seeds/Unsolved/db/seeds.sql
@@ -0,0 +1,17 @@
+INSERT INTO books (id, book_name)
+VALUES
+    (1,'The Name of the Wind'),
+    (2,"The Wise Man's Fear"),
+    (3,'Sufficiently Advanced Magic'),
+    (4,"Cephrael's Hand"),
+    (5,"The Dager of Adendigaeth");
+
+INSERT INTO categories (id, category_name)
+VALUES
+    (1, 'Fantasy'),
+    (2, 'Adventure'),
+    (3, 'Epic');
+INSERT INTO prices (id, price)
+VALUES
+    (1, 29.99),
+    (2, 24.99);
